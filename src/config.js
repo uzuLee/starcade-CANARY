a4c4d029147b400539d2deea4892f75e5ae05dea
uzu Lee <108953598+uzuLee@users.noreply.github.com>
2025-09-02 20:17:36 +0900

    배포

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT || 8080,
-  frontendUrl: process.env.FRONTEND_URL || 'https://uzulee.github.io/starcade/',
+  frontendUrl: process.env.FRONTEND_URL || 'https://uzulee.github.io',
   jwtSecret: process.env.JWT_SECRET || crypto.randomBytes(32).toString('hex'),
   cookieDomain: process.env.COOKIE_DOMAIN || 'uzulee.github.io',
 };
