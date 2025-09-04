27b4859759ec8107119ebd6753ff93da2c024693
uzu Lee <108953598+uzuLee@users.noreply.github.com>
2025-09-05 01:13:35 +0900

    배포

--- server.js
+++ server.js
@@ -17,19 +17,6 @@ const socketRepository = require('./src/repositories/socketRepository');
 
 const { loadEmailTemplatesConfig } = require('./src/emailTemplateSelector');
 
-// Import route and socket handlers
-const authRoutes = require('./src/authRoutes');
-const userRoutes = require('./src/userRoutes');
-const friendRoutes = require('./src/friendRoutes');
-const scoreRoutes = require('./src/scoreRoutes');
-const accountRoutes = require('./src/accountRoutes');
-const gameRoutes = require('./src/gameRoutes');
-const messageRoutes = require('./src/messageRoutes');
-const achievementRoutes = require('./src/achievementRoutes');
-const cosmeticsRoutes = require('./src/cosmeticsRoutes');
-const calendarRoutes = require('./src/calendarRoutes');
-const socketHandlers = require('./src/socketHandlers');
-
 const app = express();
 const corsOptions = {
     origin: [
@@ -57,9 +44,19 @@ app.get('/test-path/', (req, res) => {
 });
 
 const server = http.createServer(app);
-// const io = new Server(server, { path: '/socket.io/', cors: corsOptions });
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Explicitly handle pre-flight requests
+app.use(cookieParser());
+app.use((req, res, next) => {
+    console.log('Server.js: req.cookies =', req.cookies);
+    next();
+});
+app.use(express.json());
 
 async function startServer() {
+    const io = new Server(server, { path: '/socket.io/', cors: corsOptions }); // Moved io definition inside
+
     try {
         // Connect to Redis first
         await initRedis();
@@ -82,7 +79,7 @@ async function startServer() {
         messageRoutes(app); // Register message routes
 
         // Register socket handlers
-        // socketHandlers(io, { pubClient, subClient }, { userRepository, socketRepository });
+        socketHandlers(io, { pubClient, subClient }, { userRepository, socketRepository });
 
         const PORT = config.port;
         server.listen(PORT, () => {
@@ -95,3 +92,6 @@ async function startServer() {
 }
 
 startServer();
+
+
+
